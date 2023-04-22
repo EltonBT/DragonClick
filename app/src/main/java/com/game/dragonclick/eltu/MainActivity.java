a6658bb9cf0be2dd5b8aca377e206b6e7db2bda1
Elton <eltonryan.bt0@gmail.com>
2023-04-22 06:14:45 -0400

    feat: atualização de interfaces.

--- app/src/main/java/com/game/dragonclick/eltu/MainActivity.java
+++ app/src/main/java/com/game/dragonclick/eltu/MainActivity.java
@@ -1,29 +1,22 @@
 package com.game.dragonclick.eltu;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.game.dragonclick.eltu.DTO.PlayerScore;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton bt_start, bt_definition, bt_about, bt_end;
 
-    private DatabaseReference mDatabase;
     AudioPlay audioPlay = new AudioPlay();
 
     @Override
@@ -35,33 +28,19 @@ public class MainActivity extends AppCompatActivity {
         bt_definition = findViewById(R.id.bt_definition);
         bt_about = findViewById(R.id.bt_about);
         bt_end = findViewById(R.id.bt_end);
-        bt_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Partida.class);
-                startActivity(intent);
-            }
+        bt_start.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Partida.class);
+            startActivity(intent);
         });
-        bt_definition.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Definicoes.class);
-                startActivity(intent);
-            }
+        bt_definition.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Definicoes.class);
+            startActivity(intent);
         });
-        bt_about.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Sobre.class);
-                startActivity(intent);
-            }
+        bt_about.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Sobre.class);
+            startActivity(intent);
         });
-        bt_end.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        bt_end.setOnClickListener(v -> finish());
 
         if(getMusicConfig()){
             audioPlay.play(getApplicationContext());
@@ -87,23 +66,20 @@ public class MainActivity extends AppCompatActivity {
     public void loadBestPlayer(){
         TextView textViewBestPlayer = findViewById(R.id.textViewBestPlayerName);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
 
         try{
-            mDatabase.child("players").child("1").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DataSnapshot> task) {
-                    if (!task.isSuccessful()) {
-                        Log.e("GAME_LOG", "Error getting data", task.getException());
-                    }
-                    else {
-                        Log.d("GAME_LOG", String.valueOf(task.getResult().getValue()));
+            mDatabase.child("players").child("1").get().addOnCompleteListener(task -> {
+                if (!task.isSuccessful()) {
+                    Log.e("GAME_LOG", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("GAME_LOG", String.valueOf(task.getResult().getValue()));
 
-                        PlayerScore savedPlayerScore = task.getResult().getValue(PlayerScore.class);
+                    PlayerScore savedPlayerScore = task.getResult().getValue(PlayerScore.class);
 
-                        if(savedPlayerScore != null){
-                            textViewBestPlayer.setText(String.format("%s - %s", savedPlayerScore.name, savedPlayerScore.media));
-                        }
+                    if(savedPlayerScore != null){
+                        textViewBestPlayer.setText(String.format("%s - %s", savedPlayerScore.name, savedPlayerScore.media));
                     }
                 }
             });
